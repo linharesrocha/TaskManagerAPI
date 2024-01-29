50cd595014d89a373ad20b0f07781436cc49ee4b
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-28 21:19:14 -0300

    Adding DbContext

---
+++ Data/TaskManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = TaskManagerAPI.Models.Domain.Task;
+
+namespace TaskManagerAPI.Data
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public TaskManagerDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
