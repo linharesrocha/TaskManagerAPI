69d679681e084b17b480cbbe181cd8d938c876cd
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-28 21:07:41 -0300

    Creating model and dto

---
+++ Models/DTO/TaskDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerAPI.Models.DTO
+{
+    public class TaskDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Models/Domain/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerAPI.Models.Domain
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
