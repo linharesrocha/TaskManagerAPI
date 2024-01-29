c5c2f37d23882b99b6da7fc498ce4e00357af42b
linharesrocha <guilhermelinharesr@gmail.com>
2024-01-28 21:40:49 -0300

    Creating SQL Server Task Repository

---
+++ Repositories/SQLServerImplementation/SQLServerTaskRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Data;
+using TaskManagerAPI.Repositories.Interface;
+
+namespace TaskManagerAPI.Repositories.SQLServerImplementation
+{
+    public class SQLServerTaskRepository : ITaskRepository
+    {
+        private readonly TaskManagerDbContext dbContext;
+
+        public SQLServerTaskRepository(TaskManagerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<Models.Domain.Task> CreateAsync(Models.Domain.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Domain.Task?> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Models.Domain.Task>> GetAllAsync()
+        {
+            return await dbContext.Tasks.ToListAsync();
+        }
+
+        public Task<Models.Domain.Task> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Domain.Task?> UpdateAsync(Guid id, Models.Domain.Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
